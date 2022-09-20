970376013d0baf34115ae99177b869083d575bf1
parmis delfani <parmis.delfani84@gmail.com>
2022-09-20 15:47:39 +0430

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Library_Managment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("0 Koodak Bold", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(501, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 64);
+            this.label1.TabIndex = 0;
+            this.label1.Text = ".سلام! خوش اومدی";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("0 Koodak Bold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(400, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(357, 52);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "شما میتوانید با لاگین کردن در این قسمت وارد مدیریت کتابخانه شوید\r\n ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Font = new System.Drawing.Font("0 Koodak Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(654, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 42);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "نام کاربری";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Font = new System.Drawing.Font("0 Koodak Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(672, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 42);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "گذرواژه";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(405, 182);
+            this.name.Multiline = true;
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(217, 23);
+            this.name.TabIndex = 4;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(405, 263);
+            this.password.Multiline = true;
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(217, 23);
+            this.password.TabIndex = 5;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            this.password.KeyDown += new System.Windows.Forms.KeyEventHandler(this.password_KeyDown);
+            // 
+            // login
+            // 
+            this.login.BackColor = System.Drawing.Color.SlateBlue;
+            this.login.Font = new System.Drawing.Font("0 Koodak Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.login.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.login.Location = new System.Drawing.Point(403, 332);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(354, 41);
+            this.login.TabIndex = 6;
+            this.login.Text = "ورود ";
+            this.login.UseVisualStyleBackColor = false;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            this.login.KeyDown += new System.Windows.Forms.KeyEventHandler(this.login_KeyDown);
+            this.login.MouseUp += new System.Windows.Forms.MouseEventHandler(this.login_MouseUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("0 Koodak Bold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(401, 380);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(359, 42);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "-------------------------------";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.button1.Font = new System.Drawing.Font("0 Koodak Bold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(403, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 41);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "ادمین نیستم";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(775, 499);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Button login;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Managment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            if (name.Text == "admin" && password.Text == "admin")
+            {
+                main index = new main();
+                index.Show();
+            }
+            else if(name.Text == "" && password.Text == "")
+            {
+                MessageBox.Show("لطفا مقادیر خواسته شده را کامل کنید");
+            }
+            else
+            {
+                MessageBox.Show("  کاربر گرامی شما ادمین نیستید لطفا روی دکمه ادمین نیستم کلیک کنید تا بتوانید از امکانات نرم افزار بهره مند شوید  ");
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            client custom = new client();
+            custom.Show();
+        }
+
+        private void login_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void name_TextChanged(object sender, EventArgs e)
+        {
+            if (name.Text == "ADMIN")
+            {
+                MessageBox.Show("در نوشتن نام کاربری از حروف کوچک انگلیسی استفاده کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+            if (password.Text == "ADMIN")
+            {
+                MessageBox.Show("در نوشتن نام کاربری از حروف کوچک انگلیسی استفاده کنید ", "هشدار", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void password_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void login_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+    }
+}
